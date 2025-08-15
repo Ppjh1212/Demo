c2ee7a0a44780a388706555a13b47fda16cac3b5
jonghwan <s23061@gsm.hs.kr>
2025-08-15 11:52:24 +0900

    feat :: riverpod. state provider실습

---
+++ lib/riverpod/state_provider/state_provider_screen.dart
@@ -0,0 +1,46 @@
+import 'package:demo/riverpod/state_notifier_provider/state_notifier_provider_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final numberProvider = StateProvider(
+  (ref) => 0,
+);
+
+class StateProviderScreen extends ConsumerWidget {
+  const StateProviderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(numberProvider);
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(state.toString()),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).update(
+                      (state) => state + 1,
+                    );
+              },
+              child: Text('UP'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).update(
+                      (state) => state - 1,
+                    );
+                // ref.read(numberProvider.notifier).state =
+                //     ref.read(numberProvider.notifier).state - 1;
+              },
+              child: Text('DOWN'),
+            ),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
